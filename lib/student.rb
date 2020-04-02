d866e7abda45732eaffda98180822659203513dc
Timur Mishiev <timurmishiev@gmail.com>
2020-04-02 23:29:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,6 +26,7 @@ class Student
     insert into students (name,grade) values(?,?);
   SQL
   DB[:conn].execute(sql, self.name, self.grade)
+  @id = DB[:conn].execute("SELECT MAX(ID) AS LastID FROM students")[0][0]
   
   end 
   
